601c6660a30e7532a148ed054aa073ef945283ff
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-02-12 14:58:39 -0500

    removed react-tappable

--- src/EventCell.js
+++ src/EventCell.js
@@ -4,86 +4,70 @@ import dates from './utils/dates';
 import { accessor, elementType } from './utils/propTypes';
 import { accessor as get } from './utils/accessors';
 
-import Tappable from 'react-tappable';//
-
 let propTypes = {
-  event: React.PropTypes.object.isRequired,
-  slotStart: React.PropTypes.instanceOf(Date),
-  slotEnd: React.PropTypes.instanceOf(Date),
+    event: React.PropTypes.object.isRequired,
+    slotStart: React.PropTypes.instanceOf(Date),
+    slotEnd: React.PropTypes.instanceOf(Date),
 
-  selected: React.PropTypes.bool,
-  eventPropGetter: React.PropTypes.func,
-  titleAccessor: accessor,
-  allDayAccessor: accessor,
-  startAccessor: accessor,
-  endAccessor: accessor,
+    selected: React.PropTypes.bool,
+    eventPropGetter: React.PropTypes.func,
+    titleAccessor: accessor,
+    allDayAccessor: accessor,
+    startAccessor: accessor,
+    endAccessor: accessor,
 
-  eventComponent: elementType,
-  eventWrapperComponent: elementType.isRequired,
-  onSelect: React.PropTypes.func
+    eventComponent: elementType,
+    eventWrapperComponent: elementType.isRequired,
+    onSelect: React.PropTypes.func
 }
 
 class EventCell extends React.Component {
-  render() {
-    let {
-        className
-      , event
-      , selected
-      , eventPropGetter
-      , startAccessor, endAccessor, titleAccessor
-      , slotStart
-      , slotEnd
-      , onSelect
-      , eventComponent: Event
-      , eventWrapperComponent: EventWrapper
-      , ...props } = this.props;
+    render() {
+        let {
+            className
+            , event
+            , selected
+            , eventPropGetter
+            , startAccessor, endAccessor, titleAccessor
+            , slotStart
+            , slotEnd
+            , onSelect
+            , eventComponent: Event
+            , eventWrapperComponent: EventWrapper
+            , ...props } = this.props;
 
+        let title = get(event, titleAccessor)
+            , end = get(event, endAccessor)
+            , start = get(event, startAccessor)
+            , isAllDay = get(event, props.allDayAccessor)
+            , continuesPrior = dates.lt(start, slotStart, 'day')
+            , continuesAfter = dates.gt(end, slotEnd, 'day')
 
-    let title = get(event, titleAccessor)
-      , end = get(event, endAccessor)
-      , start = get(event, startAccessor)
-      , isAllDay = get(event, props.allDayAccessor)
-      , continuesPrior = dates.lt(start, slotStart, 'day')
-      , continuesAfter = dates.gt(end, slotEnd, 'day')
+        if (eventPropGetter)
+            var { style, className: xClassName } = eventPropGetter(event, start, end, selected);
 
-    if (eventPropGetter)
-      var { style, className: xClassName } = eventPropGetter(event, start, end, selected);
-
-    return (
-      <EventWrapper event={event}>
-        <div
-          style={{...props.style, ...style}}
-          className={cn('rbc-event', className, xClassName, {
-            'rbc-selected': selected,
-            'rbc-event-allday': isAllDay || dates.diff(start, dates.ceil(end, 'day'), 'day') > 1,
-            'rbc-event-continues-prior': continuesPrior,
-            'rbc-event-continues-after': continuesAfter
-          })}
-          onClick={(e) => onSelect(event, e)}
-        >
-            <Tappable
-                preventDefault
-                stopPropagation
-                onTap={
-                    () => {
-                        console.log('tapped');
-                    onSelect(event);
-                    }
-                }
-                className='rbc-event-content'
-                title={title}
-            >
-                <div className='rbc-event-content' title={title}>
-                    { Event
-                        ? <Event event={event} title={title}/>
-                        : title
-                    }
+        return (
+            <EventWrapper event={event}>
+                <div
+                    style={{...props.style, ...style}}
+                    className={cn('rbc-event', className, xClassName, {
+                        'rbc-selected': selected,
+                        'rbc-event-allday': isAllDay || dates.diff(start, dates.ceil(end, 'day'), 'day') > 1,
+                        'rbc-event-continues-prior': continuesPrior,
+                        'rbc-event-continues-after': continuesAfter
+                    })}
+                    onClick={(e) => onSelect(event, e)}
+                >
+                    <div className='rbc-event-content' title={title}>
+                        { Event
+                            ? <Event event={event} title={title}/>
+                            : title
+                        }
+                    </div>
                 </div>
-            </Tappable>
-        </div>
-      </EventWrapper>
-    );
-  }
+            </EventWrapper>
+        );
+    }
 }
 
 EventCell.propTypes = propTypes;
